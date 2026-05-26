52cf2042910148526b2eef8fbb45d5bad205b9d6
mjpokorny26 <mjpokorny26@gmail.com>
2026-05-26 07:32:26 -0400

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const CLIENT_ID = process.env.EBAY_CLIENT_ID;
